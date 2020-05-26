6cafd8b36a80e77d9fba6a5be5c0928fbe0f0a59
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-26 15:46:41 -0700

    Created customers videos and rentals controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/create'
+  get 'customers/index'
+  get 'customers/show'
   resources :customers, only:[:index, :show]
 
   resources :videos, only:[:index, :show, :create]
