0988211319c5e8fbdf1140e6acb29cabec84913c
Vera <lizeth.veraro@outlook.com>
2020-05-26 16:02:09 -0700

    index actions for videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,7 @@
 class VideosController < ApplicationController
   def index
+    videos = Video.all
+    render json: videos.as_json(only: [:id, :title, :overview, :release_date, :total_inventory, :available_inventory]),
+       status: :ok
   end
 end
