fc8f1ed883b12b735b877016f74259d05fcc0544
Vera <lizeth.veraro@outlook.com>
2020-05-26 15:21:12 -0700

    first test for must get index as a json response

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -2,8 +2,12 @@ require "test_helper"
 
 describe CustomersController do
   it "must get index" do
-    get customers_index_url
+    # get customers_index_url
+    # must_respond_with :success
+
+    get customers_path
     must_respond_with :success
+    expect(response.header["Content-Type"]).must_include "json"
   end
 
 end
