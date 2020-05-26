d6ad5cacc85a3e3a5c0a7a4612af8d69defec6c9
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-26 12:41:25 -0700

    Add customers fixture, tests for proper fields, empty array

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,8 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all
 
-    render json: { ok: 'success' }, status: :ok
+    render json: customers.as_json(
+      only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone, :videos_checked_out_count]
+      ), status: :ok
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -4,7 +4,37 @@ describe CustomersController do
   it "must get index" do
     get customers_path
 
-    must_respond_with :ok
+    must_respond_with :success
     expect(response.header['Content-Type']).must_include 'json'
+  end
+
+  it "will return all the proper fields for a list of customers" do
+    customer_fields =  ["name", "registered_at", "address", "city", "state", "postal_code", "phone", "videos_checked_out_count"].sort
+    # Act
+    get customers_path
+
+    # Get the body as an array or a hash
+    body = JSON.parse(response.body)
+
+    # Assert
+    expect(body).must_be_instance_of Array
+    body.each do |customer|
+      expect(customer).must_be_instance_of Hash
+      expect(customer.keys.sort).must_equal customer_fields
+    end
+  end
+
+  it "returns an empty array if no customers exist" do
+    # Arrange
+    Customer.destroy_all
+    # Act
+    get customers_path
+
+    # Get the body as an array or a hash
+    body = JSON.parse(response.body)
+
+    # Assert
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0
   end
 end
