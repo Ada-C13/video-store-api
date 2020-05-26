99a7de60fb35fa9e428f1b2d24d189430749da01
Jessica Liang <jessicursliang@gmail.com>
2020-05-26 14:56:58 -0700

    method for GETall videos, + testing

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,6 @@
 class VideosController < ApplicationController
+	def index
+		videos = Video.all.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory])
+		render json: videos, status: :ok
+	end
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 describe VideosController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "index" do 
+    it "responds with JSON and success" do
+      get videos_path
+
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it "responds with an array of video info" do
+      get videos_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+
+      body.each do |video|
+        expect(video).must_be_instance_of Hash
+        required_attrs = ["title", "overview", "release_date", "total_inventory", "available_inventory"]
+        expect(video.keys.sort).must_equal required_attrs.sort
+      end
+    end
+
+    it "will give back an empty array if no videos" do
+      Video.destroy_all
+
+      get videos_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body).must_be_empty
+    end
+  end
+
 end
