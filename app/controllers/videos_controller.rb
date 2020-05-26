5f6d7f884a2e76cc26cfdbe5040f700124265d1e
quinqu <quinruby0@gmail.com>
2020-05-26 15:23:40 -0700

    updated tests

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -12,21 +12,31 @@ class VideosController < ApplicationController
       render json: {
         "errors": [
             "Not Found"
-        ]
+          ]
         }, status: :not_found
       return 
     end 
-
     render json: video.as_json(except: [:updated_at, :created_at, :id])
   end
 
   def create 
-    # video = Video.new 
+    video = Video.new(video_params)
+    
+    if video.save 
+      render json: video.as_json(only: [:id]), status: :created
+      return 
+    else 
+      render json: {
+        "errors": [ "Not Found" ]
+        }, status: :bad_request
+      return 
+    end 
+  end 
 
+  private 
 
-    # return a hash with the video ID, too 
-
-    # render json: video.as_json(except: [:updated_at, :created_at]), status: :created
+  def video_params
+    params.require(:video).permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
   end 
   
   
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 describe VideosController do
   VIDEO_FIELDS = ["id", "title", "overview", "release_date", "total_inventory", "available_inventory"].sort
+  VIDEO_FIELDS_SHOW = ["title", "overview", "release_date", "total_inventory", "available_inventory"].sort
   describe "index" do
     it "will get all videos in json format" do
       get videos_path
@@ -29,7 +30,7 @@ describe VideosController do
         body = JSON.parse(response.body)
 
         expect(response.header["Content-Type"]).must_include 'json' 
-        expect(body.keys.sort).must_equal VIDEO_FIELDS
+        expect(body.keys.sort).must_equal VIDEO_FIELDS_SHOW
       end 
 
       it "will throw an error for a non existent video" do 
@@ -42,32 +43,32 @@ describe VideosController do
 
         expect(body).must_be_instance_of Hash
 
-        expect(body["ok"]).must_equal false
+        expect(body["errors"][0]).must_equal "Not Found"
 
       end 
     end 
 
-    # describe "create" do 
-    #   let (:video_data) {
-    #     {
-    #       video: {
-    #         title: "New Video", 
-    #         overview: "new new new new create new new new", 
-    #         release_date:  DateTime.now, 
-    #         total_inventory: 7, 
-    #         available_inventory: 3, 
+    describe "create" do 
+      let (:video_data) {
+        {
+          video: {
+            title: "New Video", 
+            overview: "new new new new create new new new", 
+            release_date:  DateTime.now, 
+            total_inventory: 7, 
+            available_inventory: 3, 
 
-    #       }
-    #     }
-    #   }
+          }
+        }
+      }
 
-    #   it "can create a new video" do 
-    #       expect {
-    #         post video_path, params :video_data
-    #     }.must_differ "Video.count", 1
+      it "can create a new video" do 
+          expect {
+            post videos_path, params: video_data
+        }.must_differ "Video.count", 1
 
-    #     must_respond_with :created
-    #   end 
+        must_respond_with :created
+      end 
 
-    # end 
+    end 
 end
