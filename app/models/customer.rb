8e14ebb3522fe3a1f7f7c4654cec183c42c0505c
Leah Hughes <leahwhughes@gmail.com>
2020-05-26 14:45:52 -0700

    adds validations for customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,12 @@
 class Customer < ApplicationRecord
   #relationships
-  #validations 
   
+  validates :name, presence: true 
+  validates :registered_at, presence: true 
+  validates :postal_code, presence: true
+  validates :address, presence: true 
+  validates :city, presence: true 
+  validates :state, presence: true 
+  validates :phone, presence: true 
+  validates :videos_checked_out_count, presence: true 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_26_212915) do
-
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "registered_at"
@@ -27,5 +27,5 @@ ActiveRecord::Schema.define(version: 2020_05_26_212915) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
+  
 end
