d2bd7b9d42c0da9b05472b0e25c6bc2080eb5c25
ChelseaC13 <chelhelg@outlook.com>
2020-05-26 11:38:53 -0700

    api made and models generated

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  # resources :videos do
+  # resources :customers do
+
+  # get "/zomg", to: " ", as: "zomg"
+
+end
