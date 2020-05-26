ba533a8465754f2477063b8bc19fea6bed268876
Vera <lizeth.veraro@outlook.com>
2020-05-26 16:02:43 -0700

    routes for videos and customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # get 'videos/index'
+  # get 'video/index'
   # get 'customers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index, :show]
+  resources :videos, only: [:index, :show]  
   
 end
