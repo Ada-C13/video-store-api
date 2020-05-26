b4f43f2976ec459389f04fb4c5c09950986d108e
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-26 16:39:30 -0700

    Wrote  index action tests for both nominal and edge cases

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,19 +1,43 @@
 require "test_helper"
 
 describe VideosController do
-  it "must get index" do
-    get videos_index_url
-    must_respond_with :success
-  end
+  VIDEO_FIELDS = ["id", "title", "release_date", "available_inventory"].sort
+  describe "index" do 
+    it "responds with JSON and success" do
+      get videos_path
 
-  it "must get show" do
-    get videos_show_url
-    must_respond_with :success
-  end
+      expect(response.header["Content-Type"]).must_include 'json'
+      must_respond_with :ok
+    end
 
-  it "must get create" do
-    get videos_create_url
-    must_respond_with :success
-  end
+    it "returns all the proper fields for a list of videos" do
+      # Act 
+      get videos_path
 
+      # Get the body of the response as an array of hash
+      body = JSON.parse(response.body)
+
+      # Assert
+      expect(body).must_be_instance_of Array
+      
+      body.each do |video|
+        expect(video).must_be_instance_of hash
+        expect(video.keys.sort).must_equal VIDEO_FIELDS
+      end
+    end
+
+    it "return an empty array if there is no video exists" do
+      Video.destroy_all
+      # Act 
+      get videos_path
+
+      # Get the body of the response as an array of hash
+      body = JSON.parse(response.body)
+      
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+
+      # 
+    end
+  end
 end
