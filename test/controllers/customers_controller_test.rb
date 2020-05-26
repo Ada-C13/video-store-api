a33a2f4a4db904d2aa342afa1df86d91ce98e764
Jessica Stone <jstone4815@gmail.com>
2020-05-26 14:56:37 -0700

    add customer fixture and tests for index action

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  CUSTOMER_FIELDS = ["id", "name", "address", "postal_code", "city", "state", "registered_at", "phone", "videos_checked_out_count"].sort
+
+  it "must get index" do
+    get customers_path
+    must_respond_with :success
+    expect(response.header['Content-Type']).must_include 'json'
+  end
+
+  it "will return all proper fields for a list of customers" do
+    get customers_path
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+
+    body.each do |customer|
+      expect(customer).must_be_instance_of Hash
+      expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+    end
+  end
+
+  it "returns an empty array if no customers exist" do
+    Customer.destroy_all
+
+    get customers_path
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0
+  end
 end
