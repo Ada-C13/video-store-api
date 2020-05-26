b4632a2b987bcceadc294eaa510a9990f8a9cabd
Olga Patrakova <olgapatrakova@outlook.com>
2020-05-26 11:51:40 -0700

    Add column videos_checked_out_count to customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_181412) do
+ActiveRecord::Schema.define(version: 2020_05_26_184930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_181412) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "videos_checked_out_count"
   end
 
   create_table "customers_videos_joins", force: :cascade do |t|
