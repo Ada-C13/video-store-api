a119cdf945a5d098d79d3c7ab8867c5b7b61d98a
Leah Hughes <leahwhughes@gmail.com>
2020-05-26 16:27:27 -0700

    adds all index tests to customer controller

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 describe CustomersController do
-  # CUSTOMER_FIELDS = ['id', 'name', 'postal_code', 'phone', 'videos_checked_out_count', 'registered_at'].sort
+  CUSTOMER_FIELDS = ['id', 'name', 'postal_code', 'phone', 'videos_checked_out_count', 'registered_at'].sort
   
   describe 'index' do
     
@@ -11,27 +11,43 @@ describe CustomersController do
       must_respond_with :success 
       
       expect(response.header['Content-Type']).must_include 'json'
+      #TODO: check_response(expected_type: Array)
     end
     
-    # it 'returns correct fields for the list of customers' do
+    it 'returns correct fields for the list of customers' do
+      get customers_path
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+      
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+      end
+      
+    end
     
-    # end
-    
-    # it 'returns an empty array if there are no customers in the database' do
-    
-    # end
+    it 'returns an empty array if there are no customers in the database' do
+      Customer.destroy_all 
+      get customers_path
+      
+      body = JSON.parse(response.body)
+      
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+      
+    end
     
   end
   
-  # describe 'show' do
-  #   it 'returns a hash with correct fields for an existing customer' do
-  
-  #   end
-  
-  #   it 'returns a 404 response with JSON for a non-existant customer' do
-  
-  #   end
-  # end
+  describe 'show' do
+    it 'returns a hash with correct fields for an existing customer' do
+      
+    end
+    
+    it 'returns a 404 response with JSON for a non-existant customer' do
+      
+    end
+  end
   
   
 end
