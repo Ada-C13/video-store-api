87bab45eab0e5284dacd8352d63251810c3ebf89
Vera <lizeth.veraro@outlook.com>
2020-05-26 15:35:43 -0700

    get rid of comments

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 describe CustomersController do
   it "must get index" do
-    # get customers_index_url
-    # must_respond_with :success
 
     get customers_path
     must_respond_with :success
