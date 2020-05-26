17ba222f6e590b71b99b5327627e9748b77c6412
cojenco <icathyouyang@gmail.com>
2020-05-26 15:09:39 -0700

    add videos#index tests

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,7 @@
 class VideosController < ApplicationController
+  def index
+    videos = Video.all.order(:title)
+
+    render json: videos.as_json(except: [:created_at, :updated_at]), status: :ok
+  end
 end
