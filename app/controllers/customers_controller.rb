2515e292fc2407e0a95157f4dbb4ca849cf76ece
Jessica Stone <jstone4815@gmail.com>
2020-05-26 14:56:15 -0700

    add index route and action for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,7 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all.order(:name)
+
+    render json: customers.as_json(except: [:created_at, :updated_at]), status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/zomg", to: "application#zomg", as: "zomg"
-  
+  resources :customers, only: [:index]
+  resources :videos, only: [:index, :show, :create]
 end
