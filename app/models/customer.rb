cb10ecbb33294984061a0ab44cb38a74798a1b08
Antonia <antonianomamiukor@gmail.com>
2020-05-26 16:44:41 -0700

    created validations and set up relationships in models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :rentals
+
+  validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
+  
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,6 @@
 class Rental < ApplicationRecord
+  belongs_to :video
+  belongs_to :customer
+
+  validates :customer_id, :videos_id, uniqueness: true
 end
