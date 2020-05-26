98a1ad7d269fe235e1729d683ce0182aa5ef55d7
Catherina <catherine.pchelina@gmail.com>
2020-05-26 15:04:51 -0700

    Add customer controller test to verify fields

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  CUSTOMER_FIELDS = ["id", "name", "registered_at", "address", "city", "state", "postal_code", "phone"].sort
+  
+  it "includes json and responds with success" do 
+    get customers_path
+
+    expect(response.header['Content-Type']).must_include 'json'
+    must_respond_with :success 
+  end
+
+  it "will return all the proper fields for a list of customers" do 
+    get customers_path 
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array 
+
+    body.each do |customer|
+      expect(customer).must_be_instance_of Hash 
+      expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+    end
+  end
+  
+  
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 describe Customer do
-  CUSTOMER_FIELDS = ["id", "name", "registered_at", "address", "city", "state", "postal_code", "phone"].sort
-  
-  it "includes json and responds with success" do 
-    get customers_path
-
-    expect(response.header['Content-Type']).must_include 'json'
-    must_respond_with :success 
-  end
-
 end
