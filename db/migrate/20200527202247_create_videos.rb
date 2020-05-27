9f02a3a489f09c1a40736547c75f05275fd43572
Katie <katie.vandervoort@gmail.com>
2020-05-27 13:32:05 -0700

    created customer, video, and rental models

---
+++ db/migrate/20200527202247_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :videos do |t|
+      t.name :title
+      t.string :overview
+      t.string :release_date
+      t.integer : total_inventory
+      t.integer :available_inventory
+
+      t.timestamps
+    end
+  end
+end
