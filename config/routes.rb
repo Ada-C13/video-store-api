c11831348756eb054353aa8e8a81661d314348e4
Nikki Vaughan <vnikki13@gmail.com>
2020-05-26 17:17:56 -0700

    tests and controller actions for Customer index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'videos/index'
-  get 'videos/show'
-  get 'videos/create'
-  get 'customers/index'
-  get 'customers/show'
   resources :customers, only:[:index, :show]
 
   resources :videos, only:[:index, :show, :create]
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,14 +1,62 @@
 require "test_helper"
 
 describe CustomersController do
-  it "must get index" do
-    get customers_index_url
-    must_respond_with :success
+  CUSTOMER_FIELDS = ["id", "name", "registered_at", "postal_code", "phone", "videos_checked_out_count"].sort
+  
+  describe 'index' do
+    it 'responds with JSON and success' do
+      get customers_path
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :ok
+    end
+
+    it 'will return all the proper fields for a list of customers' do
+      get customers_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+      end
+    end
+
+    it 'returns an empty array if no customers exist' do
+      Customer.destroy_all
+      get customers_path
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+    end
   end
 
-  it "must get show" do
-    get customers_show_url
-    must_respond_with :success
+  describe 'show' do
+    before do 
+      @customer = customers(:customer1)
+    end
+    it 'responds with JSON and success' do
+      get customer_path(@customer.id)
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+
+    it 'will return a hash with all proper fields for an existing customer' do
+      get customer_path(@customer.id)
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal CUSTOMER_FIELDS
+    end
+
+    it 'will return a 404 request with json for a non-existant customer' do
+      get customer_path(-1)
+
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body['success']).must_equal false
+      expect(body['message']).must_equal 'Not found'
+    end
   end
 
 end
