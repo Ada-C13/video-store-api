5aa99828f15a97c1724c5fc97e6756851ff36cae
Angela Nguyen <angethuy@gmail.com>
2020-05-26 23:21:10 -0700

    added validations to video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,12 @@
 class Video < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
+
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :total_inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: :available_inventory }
+  validates :available_inventory, presence: true, numericality: { only_integer: true }
+
+
 end
