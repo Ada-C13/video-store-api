5f2a0ff07b1ba874a5af2f6401d6950f1a46e03c
Haben <haben7@gmail.com>
2020-05-26 17:40:55 -0700

    added routed for Customer and Video models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :customers, only: [:index, :create, :show]
+  resources :videos, only: [:index, :create, :show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
