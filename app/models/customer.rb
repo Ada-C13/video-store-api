d11a1d777fac0ed48de8fbe1d4be426359879bb6
Haben <haben7@gmail.com>
2020-05-26 20:01:51 -0700

    added validations for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,14 @@
 class Customer < ApplicationRecord
+
+  has_many :videos
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true
+  validates :phone, numericality: true
+  validates :registered_at, presence: true
+
 end
