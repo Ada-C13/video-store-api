29a484820b28ddd9807f1f00298bdd7ff49ec25b
mulhoo <oliviamulholland94@gmail.com>
2020-05-26 18:21:49 -0700

    model/controller Customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'customers/index'
   get 'videos/index'
   resources :videos, only: [:index] 
+  resources :customers, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200527012043_create_customers.rb
@@ -0,0 +1,16 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.datetime :registered_at
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :phone
+
+      t.timestamps
+    end
+    add_index :customers, :name
+  end
+end
