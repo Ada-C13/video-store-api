09c1457638bc77ad5a663c43567b7c55898f3b0d
Haben <haben7@gmail.com>
2020-05-26 20:42:01 -0700

    added customer controller test for index

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,40 @@
 require "test_helper"
 
 describe CustomersController do
-  it "must get index" do
-    get customers_index_url
-    must_respond_with :success
+
+  REQUIRED_CUSTOMER_FIELDS = ["id", "name", "address", "city", "state", "postal_code", "phone", "registered_at"].sort
+  
+
+  def check_response(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
+
+  describe "index" do
+    it "responds with JSON and success" do
+      get customers_path
+
+      check_response(expected_type: Array)
+    end
+
+    it "responds with an array ofcustomer hashes" do
+      # Act
+      get customers_path
+
+      # Assert
+      body = check_response(expected_type: Array)
+
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+
+        expect(customer.keys.sort).must_equal REQUIRED_CUSTOMER_FIELDS
+      end
+    end
   end
 
 end
