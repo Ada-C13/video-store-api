8ba84be6ec6fc4ec7faa05d35967462ff9be1f36
luckyMonkey <freeMonkey19@gmail.com>
2020-05-26 19:59:23 -0700

    add first index action test

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 describe CustomersController do
   it "must get index" do
-    get customers_index_url
+    get customers_path
+
     must_respond_with :success
+    expect(response.header['Content-Type']).must_include 'json'
   end
 
 end
