b014a00078cd033526c41ae58bccf136c49e71cd
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-27 00:52:43 -0700

    added test to check if the index is returning all proper feelds for list of videos

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,12 +1,27 @@
 require "test_helper"
 
 describe VideosController do
+  VIDEO_FIELDS = ["id", "title", "overview", "release_date", "total_inventory", "available_inventory"]
 
   it "must get index" do
     get videos_path
 
     must_respond_with :success
     expect(response.header['Content-Type']).must_include 'json'
+  end
+
+  it "will return all the proper fields for a list of videos" do
+
+    get videos_path
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+
+    body.each do |video|
+      expect(video).must_be_instance_of Hash
+      expect(video.keys.sort).must_equal VIDEO_FIELDS
+    end
   end
 
   
