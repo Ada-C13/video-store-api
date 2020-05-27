59c7caec57adc0434bc64d5f39294aa0cb52c16c
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 14:29:52 -0700

    Added validation rules to Video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
   validates :title, presence: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :total_inventory, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :available_inventory, presence: true, numericality: { only_integer: true, greater_than: -1 }
 end
