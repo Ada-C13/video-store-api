832a084931b93503492e1987f130e3581d8e0592
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-26 22:05:10 -0700

    add model for Vidoes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_045321) do
+ActiveRecord::Schema.define(version: 2020_05_27_050426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2020_05_27_045321) do
     t.string "state"
     t.string "postal_code"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.date "release_date"
+    t.integer "total_inventory"
+    t.integer "available_inventory"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
