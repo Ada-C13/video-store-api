243958b75eef4262142cb7b4b0eb971761fe1e53
Jessica Liang <jessicursliang@gmail.com>
2020-05-26 17:04:08 -0700

    edge case testing for checkout

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -29,6 +29,59 @@ describe RentalsController do
       expect(body.keys.sort).must_equal REQUIRED_ATTRS
     end
 
+    it "will respond with bad_request if the customer is not found" do 
+      check_out_data[:rental][:customer_id] = nil
+
+      expect {
+        post check_out_path, params: check_out_data
+      }.wont_change "Rental.count"
+
+      must_respond_with :bad_request 
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body["errors"]).must_equal 'Customer does not exist.'
+    end
+
+    it "will respond with bad_request if the video is not found" do 
+      check_out_data[:rental][:video_id] = nil
+
+      expect {
+        post check_out_path, params: check_out_data
+      }.wont_change "Rental.count"
+
+      must_respond_with :bad_request 
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body["errors"]).must_equal 'Video does not exist.'
+    end
+
+    it "will respond with ok false if the video is not in stock" do 
+      check_out_data[:rental][:video_id] = videos(:none_avail_vid).id
+
+      expect {
+        post check_out_path, params: check_out_data
+      }.wont_change "Rental.count"
+
+      must_respond_with :ok 
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body["errors"]).must_equal 'This video is out of stock.'
+    end
+
+    it "will respond with bad_request if rental request is invalid" do
+      check_out_data[:rental][:video_id] = nil
+      check_out_data[:rental][:customer_id] = nil
+
+      expect {
+        post check_out_path, params: check_out_data
+      }.wont_change "Rental.count"
+
+      must_respond_with :bad_request
+      expect(response.header['Content-Type']).must_include 'json'
+    end
   end
 
   describe "check in" do
