1f911f399c096270444d2d102b679b6f0ec23a09
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-27 14:41:58 -0700

    added test that video will have required field

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -5,8 +5,14 @@ describe Video do
     @video = Video.new(title: "Somthign", overview: "Somthing", release_date: 2017-03-14, total_inventory: 4, available_inventory: 5)
   end
 
-  it "Video can be instantiated" do
+  it "video can be instantiated" do
     expect(@video.valid?).must_equal true
+  end
+
+  it "video will have the required field" do
+    [:id, :title, :overview, :release_date, :total_inventory, :available_inventory].each do |field|
+      expect(@video).must_respond_to field
+    end
   end
 
 
