41a7ac6515fe6be54be386cf2bd3f6ec83c42bc2
Jessica Stone <jstone4815@gmail.com>
2020-05-27 14:21:42 -0700

    initialize rentals and set up relationship

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,5 @@
 class Video < ApplicationRecord
+  has_many :rentals
+  has_many :customers, through: :rentals
   validates :title, :overview, :total_inventory, :available_inventory, :release_date, presence: true
 end
