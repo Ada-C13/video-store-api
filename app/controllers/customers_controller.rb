19cdd876f742ed30ba1df136f176e91ced7d6961
Vera <lizeth.veraro@outlook.com>
2020-05-26 21:42:23 -0700

    show customer action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,22 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all
     render json: customers.as_json(only: [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at, :videos_checked_out_count]),
-       status: :ok
+           status: :ok
+  end
+
+  def show
+    customer = Customer.find_by(id: params[:id])
+
+    if customer.nil?
+      render json: {
+        ok: false,
+        message: "Not found",
+      }, status: :not_found
+
+      return
+    end
+
+    render json: customer.as_json(only: [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at, :videos_checked_out_count]),
+      status: :ok
   end
 end
