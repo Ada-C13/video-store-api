443b23148845436f3a44f4a6783abd153a4cd626
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 09:18:03 -0700

    Added VideosController#show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,18 @@ class VideosController < ApplicationController
     videos = Video.all.as_json(only: [:id, :title, :release_date, :available_inventory])
     render json: videos, status: :ok
   end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video
+      render json: video.as_json(
+        only: [:title, :overview, :release_date, :total_inventory, :available_inventory]
+      )
+      return
+    else 
+      render json: { ok: false, errors: ["Not Found"] }, status: :not_found
+      return
+    end
+  end
 end
