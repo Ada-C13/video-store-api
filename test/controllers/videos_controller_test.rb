e3f9cd7c967c70ef1d0df02b36b4b7a64518e44b
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-27 00:44:52 -0700

    added test for index

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,19 +1,25 @@
 require "test_helper"
 
 describe VideosController do
+
   it "must get index" do
-    get videos_index_url
+    get videos_path
+
     must_respond_with :success
+    expect(response.header['Content-Type']).must_include 'json'
   end
 
-  it "must get show" do
-    get videos_show_url
-    must_respond_with :success
-  end
+  
 
-  it "must get create" do
-    get videos_create_url
-    must_respond_with :success
-  end
+
+  # it "must get show" do
+  #   get videos_show_url
+  #   must_respond_with :success
+  # end
+
+  # it "must get create" do
+  #   get videos_create_url
+  #   must_respond_with :success
+  # end
 
 end
