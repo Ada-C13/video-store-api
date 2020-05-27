74ba1907fb617ca9c9396ae8ac9e4e1c7cc9de24
mulhoo <oliviamulholland94@gmail.com>
2020-05-27 11:27:05 -0700

    index for controller, all tests pass

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,9 @@
 class CustomersController < ApplicationController
+  CUSTOMER_FIELDS = ["id", "name", "registered_at", "address", "city", "state", "postal_code", "phone"]
+
   def index
+    customers = Customer.all
+    
+    render json: customers.as_json(only: CUSTOMER_FIELDS), status: :ok
   end
 end
