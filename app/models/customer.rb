17c732598bf7d11b587f209a28f0527d844abad0
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 15:12:30 -0700

    Wrote validation rules for Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :registered_at, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true
+  validates :videos_checked_out_count, presence: true, numericality: { only_integer: true, greater_than: -1 }
 end
