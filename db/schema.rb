9e3e28828f5ec9118b8bbd8e926da74883c4f765
Angela Nguyen <angethuy@gmail.com>
2020-05-27 00:29:13 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_033425) do
+ActiveRecord::Schema.define(version: 2020_05_27_072536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_033425) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "customer_id"
     t.bigint "video_id"
-    t.boolean "returned", default: false
+    t.datetime "returned_on"
     t.index ["customer_id"], name: "index_rentals_on_customer_id"
     t.index ["video_id"], name: "index_rentals_on_video_id"
   end
