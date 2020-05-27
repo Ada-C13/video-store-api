2add15a651a89112b764c352bb80329d5a7b0dbd
Antonia <antonianomamiukor@gmail.com>
2020-05-27 15:17:42 -0700

    video index and show controller features working with corresponding tests. still working on create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,39 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all
+    videos = Video.all
+    render json: videos.as_json(only: [:id, :title, :release_date, :available_inventory]), status: :ok
+  end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {
+        ok: false,
+        message: 'Not found'
+      }, status: :not_found
+      return
+    end
+      render json: video.as_json(only: [:id, :title, :release_date, :available_inventory])
+    end
+
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json, status: :created
+      return
+    else
+      render json: {
+        ok: false,
+        message: 'Unable to create new video'
+      }, status: :bad_request
+      return
+    end
+  end
+
+  private
+  def video_params
+    return params.require(:video).permit(:title, :release_date, :available_inventory)
   end
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
+<<<<<<< Updated upstream
 # describe VideosController do
 #   # it "must get index" do
 #   #   get videos_index_url
@@ -7,3 +8,86 @@ require "test_helper"
 #   end
 
 # end
+=======
+describe VideosController do
+  VIDEO_FIELDS = ["id", "title", "release_date", "available_inventory"].sort
+  it "must get index" do
+    get videos_path
+    
+    must_respond_with :success
+    expect(response.header['Content-Type']).must_include 'json'
+  end
+  
+  it "will return all the proper fields for videos" do
+    
+    get videos_path
+    #gets body of response as array or hash
+    body = JSON.parse(response.body)
+    
+    expect(body).must_be_instance_of Array
+    
+    body.each do |video|
+      expect(video).must_be_instance_of Hash
+      expect(video.keys.sort).must_equal VIDEO_FIELDS
+    end
+  end
+  
+  it "returns an empty array if no videos exist" do
+    Video.destroy_all
+    
+    get videos_path
+    #gets body of response as array or hash
+    body = JSON.parse(response.body)
+    
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0
+    
+  end
+  
+  describe "show" do
+    it "will return a hash with the proper fields for an existing video" do
+      
+      video = videos(:firstvideo)
+      
+      get video_path(video.id)
+      
+      body = JSON.parse(response.body)
+      
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal VIDEO_FIELDS
+    end
+    
+    it "will return a 404 request with json for a nonexistent video" do
+      get video_path(-1)
+      
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body['ok']).must_equal false
+      expect(body['message']).must_equal 'Not found'
+    end
+  end
+
+    describe "create" do
+      let(:video_data) {
+        {
+          video: {
+            title: "Video",
+            release_date: "11/11/1111",
+            available_inventory: 13
+          }
+        }
+      }
+
+      it "can create a new video" do
+        expect {
+          post videos_path, params: video_data
+        }.must_differ "Video.count", 1
+        must_respond_with :created
+      end
+    end
+  
+end
+>>>>>>> Stashed changes
