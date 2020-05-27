f527f0ba88efb926dbc48e6d969d4c2aeb794518
Leah Hughes <leahwhughes@gmail.com>
2020-05-26 20:31:38 -0700

    stubs validation tests for customer and video

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,10 +1,34 @@
 require "test_helper"
 
 describe 'Customer' do
-  
-  # it 'can be instantiated' do
-  
-  # end
-  
-  
+  describe 'validations' do
+    it 'is invalid without a name' do
+
+    end
+
+    it 'is invalid without a postal code' do
+
+    end
+
+    it 'is invalid without an address' do
+
+    end
+
+    it 'is invalid without a city' do
+
+    end
+
+    it 'is invalid without a state' do
+
+    end
+
+    it 'is invalid without a phone number' do
+
+    end
+
+    it 'is invalid without a videos_checked_out_count' do
+
+    end
+  end
+
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    it 'is invalid without title' do
+
+    end
+
+    it 'is invalid without release date' do
+
+    end
+
+    it 'is invalid without total inventory' do
+
+    end
+
+    it 'is invalid if total inventory is not an integer' do
+
+    end
+
+    it 'is invalid without available inventory' do
+
+    end
+
+    it 'is invalid if available inventory is not an integer' do
+
+    end
+
+    it 'is invalid without overview' do
+
+    end
+
+  end
 end
