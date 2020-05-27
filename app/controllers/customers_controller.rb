906574a6c5beaa39cc1309e763dc0cbe6464a1d2
Haben <haben7@gmail.com>
2020-05-27 16:46:34 -0700

    added show test for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,40 +7,30 @@ class CustomersController < ApplicationController
   end
 
 
-  def create
-    customer = Customer.new(customer_params)
-
-    if customer.save
-      render json: customer.as_json(only: [:id]), status: :created
-      return
-    else
-      render json: {
-          ok: false,
-          errors: video.errors.messages
-        }, status: :bad_request
-      return
-    end
-  end
-
-
   def show
     customer = Customer.find_by(id: params[:id])
 
-    if custormer
+    if customer
       render json: customer.as_json(only: [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at])
       return
     else
       render json: { ok: false, errors: ["Not Found"] }, status: :not_found
       return
     end
-  end
 
+    # customer = Customer.find_by(id: params[:id])
 
+    # if customer.nil?
+    #   render json: {
+    #     # ok: false,
+    #     errors: ["Not Found"],
+    #   }, status: :not_found
 
-  private
+    #   return
+    # end
+    # render json: customer.as_json(only: [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at, :videos_checked_out_count]),
+    #   status: :ok
 
-  def customer_params
-    return params.require(:customer).permit(:name, :address, :city, :state, :postal_code, :phone, :registered_at)
   end
 
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,19 +1,16 @@
 require "test_helper"
 
 describe CustomersController do
-
   REQUIRED_CUSTOMER_FIELDS = ["id", "name", "address", "city", "state", "postal_code", "phone", "registered_at"].sort
-  
 
   def check_response(expected_type:, expected_status: :success)
     must_respond_with expected_status
-    expect(response.header['Content-Type']).must_include 'json'
+    expect(response.header["Content-Type"]).must_include "json"
 
     body = JSON.parse(response.body)
     expect(body).must_be_kind_of expected_type
     return body
   end
-
 
   describe "index" do
     it "responds with JSON and success" do
@@ -35,18 +32,44 @@ describe CustomersController do
         expect(customer.keys.sort).must_equal REQUIRED_CUSTOMER_FIELDS
       end
     end
+
+    it "will respond with an empty array when there are no customers" do
+      # Arrange
+      Customer.destroy_all
+
+      # Act
+      get customers_path
+
+      # Assert
+      body = check_response(expected_type: Array)
+      expect(body).must_equal []
+    end
+end
+  
+
+  describe "show" do
+    # Nominal case
+    it "will return a hash with the proper fields for an existing customer" do
+      customer = customers(:customer_1)
+
+      get customers_path(customer.id)
+      # act
+      must_respond_with :success
+
+      body = JSON.parse(response.body)[0]
+
+      expect(response.header["Content-Type"]).must_include "json"
+
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal ["address", "city", "id", "name", "phone","postal_code", "registered_at","state"]
+    end
+    
+    it "will return a 404 request with json for a non-existent customer" do
+      get customer_path(-1)
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body["errors"]).must_equal ["Not Found"]
+    end
   end
-
-  it "will respond with an empty array when there are no customers" do
-    # Arrange
-    Customer.destroy_all
-
-    # Act
-    get customers_path
-
-    # Assert
-    body = check_response(expected_type: Array)
-    expect(body).must_equal []
-  end
-
 end
