9efb9317f91e8ea07a7d9a1117dec30ab69c67ff
Emily Cowan <e.anderson8977@gmail.com>
2020-05-27 15:02:00 -0700

    added strong params and fleshed out create action for videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,13 +14,30 @@ class VideosController < ApplicationController
     else
       render json: {
         ok: false,
-        message: 'Not Found'
+        errors: ['Not Found']
       }, status: :not_found
       return
     end
   end
 
   def create
-    
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+        ok: false,
+        errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def video_params
+    return params.require:(video).permit(:title, :release_date, :total_inventory, :available_inventory)
   end
 end
