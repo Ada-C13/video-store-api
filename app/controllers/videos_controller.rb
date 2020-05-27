dbe5986db84f143b18f020ed121e87ffedb0c649
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-26 22:26:55 -0700

    added video index and show actions, and controller tests

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,35 @@
 class VideosController < ApplicationController
   def index
+    videos = Video.all.as_json(only: %i[id title release_date available_inventory])
+
+    if videos.nil?
+      render json: 
+      {
+        errors: 
+        [
+          "Not Found"
+        ]
+      }, status: :not_found
+      return
+    else
+      render json: videos, status: :ok
+    end
   end
+
+  def show
+    video = Video.find_by(id: params[:id])
+    if video.nil?
+      render json: 
+      {
+        errors: 
+        [
+          "Not Found"
+        ]
+      }, status: :not_found
+      return
+    end
+
+    render json: video.as_json(only: %i[title overview release_date total_inventory available_inventory]), status: :ok
+  end
+
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,9 +1,64 @@
 require "test_helper"
 
 describe VideosController do
-  it "must get index" do
-    get videos_index_url
-    must_respond_with :success
+
+  describe "index" do
+    it "must get index" do
+      get videos_path
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+  
+    it "will return all the proper fields for a list of videos" do
+      video_fields = %w[id title release_date available_inventory].sort
+      get videos_path
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+      body.each do |video|
+        expect(video).must_be_instance_of Hash
+        expect(video.keys.sort).must_equal video_fields
+      end
+    end
+  
+    it "returns and empty array if no videos exist" do
+      Video.destroy_all
+      get videos_path
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+    end
+  end
+  
+  describe "show" do
+    # nominal
+    it "will return a hash with proper fields for an existing video" do
+      video_fields = %w[title overview release_date total_inventory available_inventory].sort
+      video = videos(:la_la_land)
+      get video_path(video.id)
+      
+
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+
+      expect(response.header['Content-Type']).must_include 'json'
+
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal video_fields
+    end
+
+
+    # edge
+    it "will return a 404 request with json for a non-existant video" do
+      get video_path(-1)
+      
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      # expect(body['ok']).must_equal false
+      expect(body['errors'][0]).must_equal 'Not Found'
+    
+    end
   end
 
 end
