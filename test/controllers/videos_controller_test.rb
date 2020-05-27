cd399b2cc2c976cf5f01327da65d54d16187f092
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-27 00:57:07 -0700

    add test for if its no videos to test if index will return an empty array

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -24,6 +24,17 @@ describe VideosController do
     end
   end
 
+  it "returns an empty array if no videos exist" do
+    Video.destroy_all
+
+    get videos_path
+
+    body = JSON.parse(response.body)
+ 
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0
+  end
+
   
 
 
