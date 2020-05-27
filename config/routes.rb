a1e4eda652ed6d8499f5025b66030f8e5d3b9387
mulhoo <oliviamulholland94@gmail.com>
2020-05-27 10:39:24 -0700

    route modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'customers/index'
   get 'videos/index'
   resources :videos, only: [:index, :show] 
-  resources :customers, only: [:index]
+  resources :customers, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
