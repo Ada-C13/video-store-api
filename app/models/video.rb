974f0c7d1a5564d702f0a071ce1bc464b41f8662
Vera <lizeth.veraro@outlook.com>
2020-05-26 21:43:21 -0700

    decrease, increase and available methods

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,24 @@ class Video < ApplicationRecord
   # validates :overview, presence: true
   # validates :release_date, presence: true
 
-
   has_many :rentals, dependent: :nullify
   has_many :customers, through: :rentals
+
+  def decrease_inventory
+    self.available_inventory -= 1
+    self.save
+  end
+
+  def increase_inventory
+    self.available_inventory += 1
+    self.save
+  end
+
+  def available
+    if self.available_inventory == 0
+      return nil
+    end
+    return self
+  end
+
 end
