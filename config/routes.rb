b6c24449079421651ec17955b7d88e3650b16b49
Haben <haben7@gmail.com>
2020-05-27 16:47:45 -0700

    minor edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :customers, only: [:index, :create, :show]
+  resources :customers, only: [:index, :show]
   resources :videos, only: [:index, :create, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
