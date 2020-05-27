76bae4ae88f607d2433be0247137dbff054b6c69
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-26 22:07:19 -0700

    add contoller for Videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/create'
   get 'customers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
