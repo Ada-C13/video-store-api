a1d66ace22164f5e64e0352f84c97050f4de87c4
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-26 22:09:01 -0700

    add routes for videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'videos/index'
-  get 'videos/show'
-  get 'videos/create'
-  get 'customers/index'
+  # get 'videos/index'
+  # get 'videos/show'
+  # get 'videos/create'
+  # get 'customers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :customers, only: [:index]
+
+  resources :videos, only: [:index, :show, :create]
+
 end
