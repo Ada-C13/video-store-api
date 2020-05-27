bf49a07c5cc85ff01d5b0f989e546b40a20e326e
Haben <haben7@gmail.com>
2020-05-26 20:01:38 -0700

    added validations for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,11 @@
 class Video < ApplicationRecord
+
+  belongs_to :customer
+
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :total_inventory, presence: true
+  validates :available_inventory, presence: true
+  
 end
