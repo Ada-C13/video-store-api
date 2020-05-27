7b24373ffa77997381842b263f5c32423db3c6c1
quinqu <quinruby0@gmail.com>
2020-05-27 12:54:19 -0700

    checkout controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -12,29 +12,42 @@ class RentalsController < ApplicationController
     customer = Customer.find_by(id: params[:customer_id])
     date = DateTime.now + 1.week
     rental = Rental.new(due_date: date, customer_id: customer.id, video_id: video.id)
+  
 
+    if customer.nil? || video.nil? 
+      render json: {
+        errors: [
+            "Not Found"
+          ]
+        }, status: :not_found
+      return
+    end 
+    
+    video.reload
+    customer.reload
     
     if Rental.inventory_check_out(rental)
-      if rental.save 
-        render json: {
-          customer_id: rental.customer_id,
-          video_id: rental.video_id,
-          due_date: rental.due_date,
-          videos_checked_out_count: customer.videos_checked_out_count,
-          available_inventory: video.available_inventory
-        }
-        return 
-      else
-        render json: {
-          "errors": "Invalid customer or video ID"
-          }, status: :not_found
-        return
-      end 
-    else
-      render json: {
-        "errors": "No Inventory Available"
-        }, status: :bad_request
-      return 
+        if rental.save 
+          video.reload
+          customer.reload
+          render json: {
+            customer_id: rental.customer_id,
+            video_id: rental.video_id,
+            due_date: rental.due_date,
+            videos_checked_out_count: customer.videos_checked_out_count,
+            available_inventory: video.available_inventory
+          }, status: 200
+          return 
+         
+        else
+          render json: {
+            "errors": [
+                "Not Found"
+              ]
+            }, status: :not_found
+          return
+        end 
+        
     end 
   end 
 
