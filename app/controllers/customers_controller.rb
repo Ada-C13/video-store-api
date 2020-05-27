b4afe42456278f59911dd6f611a161c5908adc91
Emily Cowan <e.anderson8977@gmail.com>
2020-05-27 14:56:36 -0700

    fleshed out controller methods for customer index and video index/show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 class CustomersController < ApplicationController
-
   def index
-
+    customers = Customer.all.as_json(only: [:id, :name, :registered_at, :post_code, :phone, :videos_checked_out_count])
+    
+    render json: customers, status: :ok
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,26 @@
 class VideosController < ApplicationController
   def index
-
+    videos = Video.all.as_json(only: [:id, :title, :release_date, :available_inventory])
+    
+    render json: videos, status: :ok
   end
 
   def show
+    video = Video.find_by(id: params[:id])
 
+    if video
+      render json: video.as_json(only: [:title, :overview, :release_date, :available_inventory])
+      return
+    else
+      render json: {
+        ok: false,
+        message: 'Not Found'
+      }, status: :not_found
+      return
+    end
   end
 
   def create
-
+    
   end
 end
