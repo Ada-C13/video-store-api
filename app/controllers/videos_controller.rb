2bc16eb9662e1ad7d04d20875b9649b143bb86d9
vwhwang <vwhwang38@gmail.com>
2020-05-26 17:02:45 -0700

    added test for video and controller on show and index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,22 @@
 class VideosController < ApplicationController
   def index
+    videos = Video.all  
+    render json: videos.as_json(only:[:id,:title,:release_date,:available_inventory]), 
+                                status: :ok
   end
+
+  def show
+
+    video = Video.find_by(id: params[:id])
+    if video.nil?
+      render json: {errors:["Not Found"] }, status: :not_found
+      return 
+    end 
+
+    render json: video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory ])
+
+  end 
 end
+
+
+
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -2,8 +2,85 @@ require "test_helper"
 
 describe VideosController do
   it "must get index" do
-    get videos_index_url
+    get videos_path
+
     must_respond_with :success
+
+    expect(response.header['Content-Type']).must_include('json')
+    must_respond_with :ok
   end
 
+
+  it "will return a list of videos with proper fields" do 
+
+    video_fields = ["id","title","release_date", "available_inventory"].sort 
+
+    get videos_path 
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array 
+
+    body.each do |video|
+      expect(video).must_be_instance_of Hash 
+      expect(video.keys.sort).must_equal video_fields
+    end 
+  end 
+
+  it "will return empty array and 200 status if no video" do 
+
+    Video.destroy_all
+
+    get videos_path 
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0 
+    must_respond_with :ok
+    
+  end 
+
+  describe 'show' do 
+
+    it "will get show " do 
+      video = Video.all.first
+
+      get video_path(video.id)
+
+      must_respond_with :ok 
+      expect(response.header['Content-Type']).must_include('json')
+
+    end 
+
+    it "will return a pet " do 
+      video = Video.all.first
+      fields = ["title", "overview","release_date","total_inventory","available_inventory"].sort
+
+      get video_path(video.id)
+
+      must_respond_with :ok 
+      expect(response.header['Content-Type']).must_include('json')
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Hash 
+      expect(body["title"]).must_equal "testvideo"
+
+    end 
+
+    it "will show not_fond and error if no video" do 
+      get video_path(-1)
+
+      body = JSON.parse(response.body)
+
+      must_respond_with :not_found
+      expect(body).must_be_instance_of Hash
+
+
+      expect(body["errors"]).must_include "Not Found"
+    end 
+  end 
+
 end
+
+
