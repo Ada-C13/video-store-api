e45dd4f57cd9bb9baff7878b7e058fea0bf3b37f
Vera <lizeth.veraro@outlook.com>
2020-05-26 19:16:44 -0700

    video has_many rentals and customers

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,9 @@
 class Video < ApplicationRecord
+  # validates :title, presence: true
+  # validates :overview, presence: true
+  # validates :release_date, presence: true
+
+
+  has_many :rentals, dependent: :nullify
+  has_many :customers, through: :rentals
 end
