d043d13b9201a07393b30fdc9cd1f95810433b24
Jessica Stone <jstone4815@gmail.com>
2020-05-27 14:37:41 -0700

    add validation to customer and video

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,5 @@ class Video < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
   validates :title, :overview, :total_inventory, :available_inventory, :release_date, presence: true
+  validates :total_inventory, :available_inventory, numericality: { only_integer: true }
 end
