486bc2fd7275bb3cead58dc72ec9fe47ca0199a6
Katie <katie.vandervoort@gmail.com>
2020-05-27 13:46:46 -0700

    defined relationships between cutomers, videos, and rentals, and added registered_at to customers

--- db/migrate/20200527202059_create_customers.rb
+++ db/migrate/20200527202059_create_customers.rb
@@ -2,11 +2,12 @@ class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
       t.string :name
-      t.string : address
+      t.string :address
       t.string :city
       t.string :state
-      t.integer :postal_code
-      t.string videos_checked_out_count :phone
+      t.string :postal_code
+      t.integer :videos_checked_out_count 
+      t.string :phone
 
       t.timestamps
     end
--- db/migrate/20200527202247_create_videos.rb
+++ db/migrate/20200527202247_create_videos.rb
@@ -1,10 +1,10 @@
 class CreateVideos < ActiveRecord::Migration[6.0]
   def change
     create_table :videos do |t|
-      t.name :title
+      t.string :title
       t.string :overview
       t.string :release_date
-      t.integer : total_inventory
+      t.integer :total_inventory
       t.integer :available_inventory
 
       t.timestamps
