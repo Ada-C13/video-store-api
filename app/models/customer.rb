f3e786e0384e8ececee356d6d1ea243d2d838172
Vera <lizeth.veraro@outlook.com>
2020-05-26 19:16:08 -0700

    customer relationships has_mamy rentals and videos

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
-  
+  # validates :name, presence: true
+  # validates :registered_at, presence: true
+  # validates :address, presence: true
+  # validates :city, presence: true
+  # validates :state, presence: true
+  # validates :postal_code, presence: true
+  # validates :phone, presence: true
+
+  has_many :rentals, dependent: :nullify
+  has_many :videos, through: :rentals
 end
