af7abbca3f0f30f0135b90d40552b4d88b2ec74c
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-27 13:27:28 -0700

    Added create action in Video controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -20,4 +20,25 @@ class CustomersController < ApplicationController
     render json: customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :videos_checked_out_count]),
                                   status: :ok
   end
+
+  def create
+    video = Video.new(pet_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+        }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def video_params
+    return params.require(:video).permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
 end
