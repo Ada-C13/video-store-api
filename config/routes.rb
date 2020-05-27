312e8529f4a86d2383dfb70c1879a413a3d96cb3
Katie <katie.vandervoort@gmail.com>
2020-05-27 14:04:20 -0700

    added customers index route and method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'videos/index'
-  get 'videos/show'
-  get 'videos/create'
-  get 'customers/index'
+  # get 'videos/#index'
+  # get 'videos/#show'
+  # get 'videos/#create'
+  # get 'customers/#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :customers, only: [:index]
   # get '/zomg', to: 'application#show'
 
 end
