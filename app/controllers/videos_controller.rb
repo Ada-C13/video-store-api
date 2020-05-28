363c49eaab5e99b392f51b3aa3d533ba58c39a95
Hannah Tekié <hannahtekie@gmail.com>
2020-05-27 19:57:16 -0700

    added show action for video, and wrote the index test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,15 +1,14 @@
 class VideosController < ApplicationController
 
   def index
-
-    videos = Video.all.as_json(only: [:id, :title, :overview, :release_date, :total_inventory, :available_inventory])
-    render json: videos, status: :ok
-        
-  end
+    videos = Video.all
+    render json: videos.as_json(only: [:id, :title, :overview, :release_date, :total_inventory, :available_inventory]),
+      status: :ok
+   end      
+  
 
   def create
     video = Video.new(video_params)
-
     if video.save
       render json: vidoe.as_json(only: [:id]), status: :created
       return
@@ -26,32 +25,24 @@ class VideosController < ApplicationController
   def show
     video = Video.find_by(id: params[:id])
 
-    if video
-      render json: video.as_json(only: [:id, :title, :overview, :release_date, :total_inventory, :available_inventory])
+    if video.nil?
+      render json: {
+        ok: false,
+        messages: "not found"
+      }, status: :not_found
       return
-    else
-      render json: { ok: false, errors: ["Not Found"] }, status: :not_found
+      else
+    render json: video.as_json(only: [:id, :title, :overview, :release_date, :total_inventory, :available_inventory]),
+      status: :ok
       return
-    end
   end
 
-  # GET /videos/new
-  def new
-    @video = Video.new
-  end
-  
-  def destroy
-    @work.destroy
-    flash[:success] = "Successfully destroyed #{@work.category} #{@work.id}"
-    redirect_to root_path
-      
-  end
-  
-  
+
+
   private
 
   def video_params
     return params.require(:video).permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
   end
-
+end 
 end
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,9 +1,72 @@
 require "test_helper"
 
 describe VideosController do
-  it "must get index" do
-    get videos_index_url
-    must_respond_with :success
+
+  VIDEO_FIELDS = ["id", "title", "overview", "release_date", "total_inventory", "available_inventory"].sort
+  let(:video) {videos(:test)}
+
+  describe "index" do
+    it "list all the videos" do
+      get videos_path
+      must_respond_with :success
+    end
+
+    it "return json" do
+      get videos_path
+      expect (response.header["Content-Type"]).must_include "json"
+    end
+
+    it "return an Array" do
+      get videos_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all the proper fields for the videos" do
+      get videos_path
+
+      body = JSON.parse(response.body)
+      body.each do |video|
+        expect(video).must_be_instance_of Hash
+        expect(video.keys.sort).must_equal VIDEO_FIELDS
+      end
+    end
+
+    it "returns all the videos" do
+      get videos_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Video.count
+    end
+
+    it "return empty array if there is 0 customers" do
+      Video.destroy_all
+
+      get videos_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+    end
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
   end
 
 end
