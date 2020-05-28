611d987780cabce74780277e7269af1e42cb80dd
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-28 06:56:54 -0700

    Added the core code for RentalsController#create

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,34 @@
 class RentalsController < ApplicationController
+  def create
+    rental = Rental.new(rental_params)
+
+    if rental.save
+      rental.customer.videos_checked_out_count += 1
+      rental.customer.save
+      rental.video.available_inventory -= 1
+      rental.video.save
+      
+      render json: {
+        customer_id: rental.customer_id,
+        video_id: rental.video_id,
+        due_date: rental.due_date,
+        videos_checked_out_count: rental.customer.videos_checked_out_count,
+        available_inventory: rental.video.available_inventory
+      }, status: :ok
+    else
+      render json: {
+          ok: false,
+          errors: rental.errors.messages
+        }, status: :bad_request
+    end
+  end
+
+  private
+
+  def rental_params
+    return params.permit(:video_id, :customer_id)
+  end
+
+  # def require_customer
+  #   @customer = Customer.find_by()
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  def check_response(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
+  describe "create" do
+    let(:rental_data) {
+      {
+        video_id: videos(:frozen).id,
+        customer_id: customers(:nataliya).id
+      }
+    }
+
+    it "can create a rental" do
+      expect{post rentals_path, params: rental_data}.must_differ "Rental.count", 1
+      check_response(expected_type: Hash)
+    end
+
+    it "increase the customer's videos_checked_out_count by one" do
+      expect(customers(:nataliya).videos_checked_out_count).must_equal 6
+      
+      post rentals_path, params: rental_data
+      expect(customers(:nataliya).videos_checked_out_count).must_equal 7
+    end
+
+
+      
+    end
+  end
 end
