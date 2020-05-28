81704948a0a2438b95a09691a54ae57d68ce966b
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 19:09:45 -0700

    Set default value for Rental.active to true in the model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,10 @@
 class Rental < ApplicationRecord
   belongs_to :video
   belongs_to :customer
+
+  before_save :default_values
+  def default_values
+    self.active = true if self.active.nil? 
+    #self.due_date ||= self.created_at + 7.day # but created_at is nil! :(
+  end
 end
