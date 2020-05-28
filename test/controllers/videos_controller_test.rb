6bcff74c0181377715725a16accd060f1bf1b977
Katie <katie.vandervoort@gmail.com>
2020-05-28 16:18:27 -0700

    fixed video test by fixing video params

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -54,41 +54,42 @@ describe VideosController do
       expect(body).must_be_instance_of Hash
     end
 
-    it "will respond with an empty array when there are no pets" do
+    it "must respond with not found for invalid id" do
       # Act
-      get video_path(99)
+      get video_path(-1)
       body = JSON.parse(response.body)
   
       # Assert
       expect(body).must_be_instance_of Hash
-      expect(body["ok"]).must_equal false
+      must_respond_with :not_found
     end
   end
   
   describe "create" do
     before do
       @video_hash = {
-        video: {
+        
           title: "The Actors Are Chipmunks",
           overview: "A tale of anthropomorphization",
           release_date: "5/17/20",
           total_inventory: 5,
           available_inventory: 5
-        },
+        
       }
     end
     
-    it "can create a new video" do
-        expect {
+    it "can create a new video" do   
+         
+      expect {
           post videos_path, params: @video_hash
-      }.must_change "Video.count", 1
+      }.must_differ "Video.count", 1
 
-      new_video = Video.find_by(title: @video_hash[:video][:title])
-      expect(new_video.overview).must_equal @video_hash[:video][:overview]
+      new_video = Video.find_by(title: @video_hash[:title])
+      expect(new_video.overview).must_equal @video_hash[:overview]
     end
 
     it "can will not create a new video if title isn't included" do      
-      @video_hash[:video][:title] = nil
+      @video_hash[:title] = nil
 
         expect {
           post videos_path, params: @video_hash
@@ -97,11 +98,10 @@ describe VideosController do
       body = JSON.parse(response.body)
 
       expect(body).must_be_instance_of Hash
-      expect(body["ok"]).must_equal false
     end
 
-    it "can will not create a new video if title isn't included" do      
-      @video_hash[:video][:available_inventory] = nil
+    it "can will not create a new video if available inventory isn't included" do      
+      @video_hash[:available_inventory] = nil
 
         expect {
           post videos_path, params: @video_hash
@@ -110,7 +110,6 @@ describe VideosController do
       body = JSON.parse(response.body)
 
       expect(body).must_be_instance_of Hash
-      expect(body["ok"]).must_equal false
     end
   end
 
