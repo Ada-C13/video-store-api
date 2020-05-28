3156a3dfb170a66f0deb7f52372ff022d09d2f41
Haben <haben7@gmail.com>
2020-05-27 22:25:51 -0700

    editedd the index action's required fields

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
 
-  def index
-    customers = Customer.all.as_json(only: [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at])
+  def index 
+    customers = Customer.order(:name).as_json(only: [:id, :name, :postal_code, :phone, :registered_at, :videos_checked_out_count])
     render json: customers,  status: :ok
                                    
   end
@@ -17,19 +17,6 @@ class CustomersController < ApplicationController
       render json: { ok: false, errors: ["Not Found"] }, status: :not_found
       return
     end
-
-    # customer = Customer.find_by(id: params[:id])
-
-    # if customer.nil?
-    #   render json: {
-    #     # ok: false,
-    #     errors: ["Not Found"],
-    #   }, status: :not_found
-
-    #   return
-    # end
-    # render json: customer.as_json(only: [:id, :name, :address, :city, :state, :postal_code, :phone, :registered_at, :videos_checked_out_count]),
-    #   status: :ok
 
   end
 
