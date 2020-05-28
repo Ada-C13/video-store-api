96c4edfd024f57dbe8ec7bd3960fc0e2382c4503
kate myer <kate.d.mangubat@gmail.com>
2020-05-27 21:57:58 -0700

    added routes for check-in, check-out

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   # get 'customers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :videos, only: [:index, :show, :create]
+  
   resources :customers, only: [:index]
 
-  resources :videos, only: [:index, :show, :create]
+  post '/rentals/check-out', to: 'rentals#check_out', as: 'check_out'
+  post '/rentals/check-in', to: 'rentals#check_in', as: 'check_in'
 
-  resources :rentals, only: [:index]
+  # resources :rentals, only: [:index]
 end
