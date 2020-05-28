4376e063d27052834ce8a1690013bd8e459f33ae
cojenco <icathyouyang@gmail.com>
2020-05-27 20:11:23 -0700

    add rentals#checkout and custom methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,9 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :videos, through: :rentals
   validates :name, :address, :city, :state, :postal_code, :phone, presence: true
+
+  def increase_videos_checked_out_count
+    self.videos_checked_out_count += 1
+    self.save
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,13 @@ class Video < ApplicationRecord
   has_many :customers, through: :rentals
   validates :title, :overview, :total_inventory, :available_inventory, :release_date, presence: true
   validates :total_inventory, :available_inventory, numericality: { only_integer: true }
+
+  def check_availability
+    return self.available_inventory > 0
+  end
+
+  def decrease_inventory
+    self.available_inventory -= 1
+    self.save
+  end
 end
