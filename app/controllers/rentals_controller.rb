75bdc092489dc47d32ff75ca5abb3381846c7d14
Katie <katie.vandervoort@gmail.com>
2020-05-27 17:01:57 -0700

    created the check out route and method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,50 @@
 class RentalsController < ApplicationController
+  def check_out
+    video = Video.find_by(id: params[:rental][:video_id])
+    customer = Customer.find_by(id: params[:rental][:customer_id])
+    rental = Rental.new(customer: customer, video: video, due_date: (Date.today + 7))
+
+    if !video.nil? && video.available_inventory <= 0 
+      render json: {
+        ok: false,
+        "errors": ["Video does not have any available inventory"],
+        status: :not_found
+      }
+    elsif rental.save 
+      customer.videos_checked_out_count += 1
+      customer.save
+      video.available_inventory -= 1
+      video.save
+      render json: rental, status: :ok
+    else
+      render json: {
+        ok: false,
+        errors: [rental.errors.messages],
+        status: :bad_request
+      }
+      return
+    end
+
+  end
+
 end
+
+
+# elsif video.nil? && customer.nil?
+#   render json: {
+#     ok: false,
+#     "errors": ["Video and customer not valid"],
+#     status: :not_found
+#   }
+# elsif video.nil?
+#   render json: {
+#     ok: false,
+#     "errors": ["Video not valid"],
+#     status: :not_found
+#   }
+# elsif customer.nil?
+#   render json: {
+#     ok: false,
+#     "errors": ["Customer not valid"],
+#     status: :not_found
+#   }
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index]
   resources :videos, only: [:index, :show, :create]
+  post '/rentals/check-out', to: 'rentals#check_out'
   # get '/zomg', to: 'application#show'
 
 end
