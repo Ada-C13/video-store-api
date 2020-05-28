f6358e06780572d27093b46be06801789bd7e05a
Hannah Tekié <hannahtekie@gmail.com>
2020-05-27 21:34:19 -0700

    video show action test passed

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe VideosController do
 
   VIDEO_FIELDS = ["id", "title", "overview", "release_date", "total_inventory", "available_inventory"].sort
-  let(:video) {videos(:test)}
+  let(:video) {videos(:video_1)}
 
   describe "index" do
     it "list all the videos" do
@@ -50,7 +50,51 @@ describe VideosController do
       expect(body).must_be_instance_of Array
       expect(body.length).must_equal 0
     end
+  end
 
+  describe "show" do
+    it "will show one video" do
+      get videos_path(video)
+      must_respond_with :success
+    end
+
+    it "returns a Hash w/the right fields for existing video" do
+      video = videos(:video_1)
+
+      get video_path(video.id)
+
+      must_respond_with :success
+
+      body = JSON.parse(response.body)
+
+      expect(response.header["Content-Type"]).must_include "json"  
+
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal VIDEO_FIELDS
+    end
+
+    it "will give error for inexistent video" do
+      get video_path(-1)
+      must_respond_with :not_found
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_include "errors"
+      expect(body["errors"][0]).must_equal "Not Found"
+    end
+  end
+
+  # describe "create" do
+  #   let(:video_data){
+  #     {
+  #       title: "Petty worman",
+  #       overview: "about a girl's dream",
+  #       release_date: 1985
+  #       available_inventory:
+  #       total_inventory: 
+  #     }
+  #   }
+  # end
 
 
     
@@ -67,6 +111,6 @@ describe VideosController do
 
 
 
-  end
+  
 
 end
