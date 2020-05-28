73d0ac7f2e77b519efd7db1eba7be1dbdb88c885
Antonia <antonianomamiukor@gmail.com>
2020-05-27 21:05:33 -0700

    added inventory methods to video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,20 @@ class Video < ApplicationRecord
   has_many :customers
 
   validates :title, :release_date, :available_inventory, presence: true
+
+
+  def decrease_inventory
+    self.available_inventory -= 1
+    self.save
+  end
+
+  def increase_inventory
+    self.available_inventory += 1
+    self.save
+  end
+
+  def available
+    return self.available_inventory > 0
+  end
+
 end
