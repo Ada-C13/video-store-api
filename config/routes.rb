4b39b8d97f967d35e19f0f420428233b824ae1af
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-28 16:34:07 -0700

    tested index action for Videos and Customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-end
-=======
   resources :customers, only: [:index]
 
   resources :videos, only: [:index, :show, :create]
@@ -10,4 +6,4 @@ end
   post "/rentals/check-out/:customer_id/:video_id", to: "rentals#check_out", as: "check_out"
   post "/rentals/check-in/:customer_id/:video_id", to: "rentals#check_in", as: "check_in"
 end
->>>>>>> fbc4c053052ecbafb32f403b34358b5d775fc6cf
+
