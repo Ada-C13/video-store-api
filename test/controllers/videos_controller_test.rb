db841d751b9007b9e187afbc17c3307fd395dfa7
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-28 16:55:24 -0700

    testing index action for returning an array of hashes for Videos and Customers

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -7,4 +7,29 @@ describe VideosController do
     must_respond_with :ok
     expect(response.header['Content-Type']).must_include 'json' 
   end
+  it "will return all the proper fields a list of videos" do
+    video_fields = ["id", "title", "release_date", "available_inventory"].sort
+
+    get videos_path
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    body.each do |video|
+      expect(video).must_be_instance_of Hash
+      expect(video.keys.sort).must_equal video_fields
+    end
+  end
+  it "will return an empty array if no videos exist" do 
+    Video.destroy_all
+
+
+    get videos_path
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0 
+
+  end
 end
