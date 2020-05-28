c7b36c48ef7409a03559afee67b38106293be5a8
luckyMonkey <freeMonkey19@gmail.com>
2020-05-27 23:43:38 -0700

    clarify resources and add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-resources :videos
-resources :customers, only: [:index, :show]
-resources :rentals
+  resources :videos, only: [:index, :show, :new, :create]
+  resources :customers, only: [:index, :show]
+  resources :rentals, only: [:index]
+
+
+  post "/rentals/check-out", to: "rentals#check_out", as: "check_out"
+  post "/rentals/check-in", to: "rentals#check_in", as: "check_in"
 end
