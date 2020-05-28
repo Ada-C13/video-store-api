a2c13742ad3c6cb1b6e572898c865380e68a0f39
Haben <haben7@gmail.com>
2020-05-27 22:28:27 -0700

    added check-out and check-in  to routes and made video as root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :customers, only: [:index, :create, :show]
   resources :videos, only: [:index, :create, :show]
 
+  post "/rentals/check-out", to: "rentals#check_out", as: "check-out"
+  post "/rentals/check-in", to: "rentals#check_in", as: "check-in"
+
+  root "videos#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
