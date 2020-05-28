f29f9b36524d1e6ecd5ef7048b1afded233ded74
Yesenia <yesentorres@gmail.com>
2020-05-28 13:22:53 -0700

    added new validations to video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ApplicationRecord
     has_many :rentals
 
     validates :title, presence: true
+    validates :overview, presence: true
+    validates :release_date, presence: true
+    validates :total_inventory, presence: true
+    validates :available_inventory, presence: true
 end
