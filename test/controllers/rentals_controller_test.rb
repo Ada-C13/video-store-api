2215c90ddcea546a471f32ce660382d55fd5ead8
luckyMonkey <freeMonkey19@gmail.com>
2020-05-27 22:49:39 -0700

    add tests for check_in method

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -4,4 +4,25 @@ describe RentalsController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
+
+
+
+
+
+  describe "check_in" do 
+    let(:rental_data) {
+      {
+        customer_id: customers(:charli).id
+        video_id: videos(:firstvideo).id
+      }
+    }
+
+    before do
+      post check_out_path, params: rental_data
+  end
+
+  it "will allow a video check in" do
+    customer_video_count = customers(:charli).videos_checked_out_count
+    video_inventory = videos(:firstvideo).available_inventory
+  end
 end
