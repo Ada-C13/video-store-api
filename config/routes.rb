cb8a2d0b6b36c580e59ab3fe625b85e5e6cbf703
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-28 13:12:41 -0700

    Added routes for rentals to check in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :videos, only: [:index, :show, :create]
   resources :customers, only: [:index]
-  resources :rentals, only: [:create]
+  resources :rentals, only: [:create, :destroy]
 
   post "/rentals/check-out", to: "rentals#create", as: "check-out"
+  post "/rentals/check-in", to: "rentals#destroy", as: "check-in"
 end
