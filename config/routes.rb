3dfa714a7a5c2790b7fb7cbb7f7c82bd392663b4
Katie <katie.vandervoort@gmail.com>
2020-05-28 16:44:58 -0700

    added check in and check out tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index]
   resources :videos, only: [:index, :show, :create]
-  post '/rentals/check-out', to: 'rentals#check_out'
-  post '/rentals/check-in', to: 'rentals#check_in'
+  post '/rentals/check-out', to: 'rentals#check_out', as: 'check-out'
+  post '/rentals/check-in', to: 'rentals#check_in', as: 'check-in'
   # get '/zomg', to: 'application#show'
 
 end
