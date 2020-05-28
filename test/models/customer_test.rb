15c40c424efed262cd076b51eb1c5b5888501828
Yesenia <yesentorres@gmail.com>
2020-05-28 14:15:54 -0700

    added new customer model validations and tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do 
+    it "is valid when name is present" do 
+      # Act
+      result = customers(:shelley).valid?
+
+      # Assert
+      expect(result).must_equal true
+    end 
+
+    it "is not valid when name is absent" do 
+    # Arrange
+    invalid_customer = Customer.new(name: nil)
+
+    # Act
+    result = invalid_customer.valid?
+  
+    # Assert
+    expect(result).must_equal false
+    expect(invalid_customer.errors.messages).must_include :name
+    end 
+
+    it "is not valid when phone number is absent" do 
+      # Arrange
+      invalid_customer = Customer.new(name: "Tom", phone: nil)
+
+      # Act
+      result = invalid_customer.valid?
+    
+      # Assert
+      expect(result).must_equal false
+      expect(invalid_customer.errors.messages).must_include :phone
+    end 
+  end
 end
