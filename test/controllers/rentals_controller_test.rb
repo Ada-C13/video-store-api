69d50fa5dd30888db3750686b8abbed30479c522
Catherina <catherine.pchelina@gmail.com>
2020-05-27 22:31:46 -0700

    Move validation tests to rental_test file

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -140,44 +140,4 @@ describe RentalsController do
       expect(body["errors"]).must_equal ["Not Found"]
     end
   end
-
-  describe "validations" do 
-    before do 
-      @rental = Rental.create(
-        customer_id: customers(:customer_one).id,
-        video_id: videos(:fake_vid).id,
-        due_date: Date.today + 7
-      )
-    end
-
-    it "is valid when all fields are present" do 
-      result = @rental.valid?
-
-      expect(result).must_equal true
-    end
-
-    it "is invalid without a customer_id" do 
-      @rental.customer_id = nil 
-
-      result = @rental.valid?
-
-      expect(result).must_equal false
-    end
-
-    it "is invalid without a video_id" do 
-      @rental.video_id = nil 
-
-      result = @rental.valid?
-
-      expect(result).must_equal false
-    end
-
-    it "is invalid without a due_date" do 
-      @rental.due_date = nil 
-
-      result = @rental.valid?
-
-      expect(result).must_equal false
-    end
-  end
 end
