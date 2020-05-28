00e70ed45e2f2360401918ef07de3fa072030330
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 20:03:28 -0700

    Added validation tests for Rental model

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    let(:rental_data) {
+      {
+        video_id: videos(:frozen).id,
+        customer_id: customers(:nataliya).id 
+      }
+    }
+
+    it "is valid when video_id and customer_id are present" do
+      rental = Rental.new(rental_data)
+      expect(rental.valid?).must_equal true
+    end
+
+    it "is invalid without a video" do
+      rental_data[:video_id] = nil
+      rental = Rental.new(rental_data)
+      expect(rental.valid?).must_equal false
+    end
+
+    it "is invalid without a customer" do
+      rental_data[:customer_id] = nil
+      rental = Rental.new(rental_data)
+      expect(rental.valid?).must_equal false
+    end
+  end
 end
