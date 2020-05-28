ce72750e5542329d5a115b13c1551c6e89553bed
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 20:23:40 -0700

    Added routes for checking-out

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :videos, only: [:index, :show, :create]
   resources :customers, only: [:index]
+  resources :rentals, only: [:create]
+
+  post "/rentals/check-out", to: "rentals#create", as: "check-out"
 end
