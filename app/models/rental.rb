b2e8b575b8fea37977e1e1db94fcb01f1a1ce060
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 19:38:58 -0700

    Added default due_date calc to Rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,14 +2,14 @@ class Rental < ApplicationRecord
   belongs_to :video
   belongs_to :customer
 
-  validates :work_id, presence: true
+  validates :video_id, presence: true
   validates :customer_id, presence: true
-  validates :due_date, presence: true
-  validates :active, presence: true
+  # validates :due_date, presence: true
+  # validates :active, presence: true
 
   before_save :default_values
   def default_values
     self.active = true if self.active.nil? 
-    #self.due_date ||= self.created_at + 7.day # but created_at is nil! :(
+    self.due_date = Date.today + 7.day if self.due_date.nil?
   end
 end
