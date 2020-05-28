e31a493d7e71f42d1d105068ae4a8a26ba8540a9
Yesenia <yesentorres@gmail.com>
2020-05-28 14:51:01 -0700

    added validation tests for Video model

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do 
+    it "is valid when all fields are present" do 
+      # Act
+      result = videos(:joker).valid?
+
+      # Assert
+      expect(result).must_equal true
+    end 
+
+    it "is not valid when any field is absent" do 
+      # Arrange
+      invalid_video = Video.new()
+
+      # Act
+      result = invalid_video.valid?
+
+      # Assert
+      expect(result).must_equal false
+      expect(invalid_video.errors.messages).must_include :title
+      expect(invalid_video.errors.messages).must_include :overview
+      expect(invalid_video.errors.messages).must_include :release_date
+      expect(invalid_video.errors.messages).must_include :total_inventory
+      expect(invalid_video.errors.messages).must_include :available_inventory
+    end 
+  end
 end
