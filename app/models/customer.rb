b0d9d0568046d6ff887008e097c108d9aaf60477
luckyMonkey <freeMonkey19@gmail.com>
2020-05-27 20:26:50 -0700

    create add/remove video checked in/out methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,15 @@
 class Customer < ApplicationRecord
-  has_many :rentals
-
   validates :name, :registered_at, :postal_code, :phone, presence: true
-  
-end
+
+  has_many :rentals
+  has_many :videos through: :rentals
+
+  def add_videos_to_checked_out
+    self.videos_checked_out_count += 1
+    self.save
+  end
+
+  def remove_videos_from_checked_out
+    self.videos_checked_out_count -= 1
+    self.save
+  end
