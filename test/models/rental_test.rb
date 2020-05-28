5419fc10a51e8901625b6de2bc0331b356911fa1
cojenco <icathyouyang@gmail.com>
2020-05-27 21:26:14 -0700

    add rental model tests for relations

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do
+    before do
+      @customer = customers(:jessica)
+      @video = videos(:jurassicpark)
+      @rental = Rental.create!(customer_id: @customer.id, video_id: @video.id)
+    end
+    
+    it "belongs to one video" do
+      expect(@rental.video).must_be_instance_of Video
+      expect(@rental.video).must_equal @video
+    end
+
+    it "belongs to one customer" do
+      expect(@rental.customer).must_be_instance_of Customer
+      expect(@rental.customer).must_equal @customer
+    end
+  end
 end
