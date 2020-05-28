f49f7113ad0326eedf9e2f0cbc39f2f916846532
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 20:10:41 -0700

    Added tests for default_values of Rental model

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,14 +1,14 @@
 require "test_helper"
 
 describe Rental do
-  describe "validations" do
-    let(:rental_data) {
-      {
-        video_id: videos(:frozen).id,
-        customer_id: customers(:nataliya).id 
-      }
+  let(:rental_data) {
+    {
+      video_id: videos(:frozen).id,
+      customer_id: customers(:nataliya).id 
     }
+  }
 
+  describe "validations" do
     it "is valid when video_id and customer_id are present" do
       rental = Rental.new(rental_data)
       expect(rental.valid?).must_equal true
@@ -24,6 +24,22 @@ describe Rental do
       rental_data[:customer_id] = nil
       rental = Rental.new(rental_data)
       expect(rental.valid?).must_equal false
+    end
+  end
+
+  describe "default_values" do
+    it "sets default value for active to true" do
+      rental = Rental.new(rental_data)
+      rental.save
+
+      expect(rental.active).must_equal true
+    end
+
+    it "sets default due_date to 7 days from the current date" do
+      rental = Rental.new(rental_data)
+      rental.save
+
+      expect (rental.due_date).must_equal Date.today + 7.day
     end
   end
 end
