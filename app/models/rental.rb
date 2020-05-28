ec67590efcfe539f245462fa36a69df01f9e5880
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-27 19:11:38 -0700

    Added validation rules to Rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,6 +2,11 @@ class Rental < ApplicationRecord
   belongs_to :video
   belongs_to :customer
 
+  validates :work_id, presence: true
+  validates :customer_id, presence: true
+  validates :due_date, presence: true
+  validates :active, presence: true
+
   before_save :default_values
   def default_values
     self.active = true if self.active.nil? 
