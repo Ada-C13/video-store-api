d10639f6b9b58d8c3258faefd5f8f9e86e40670f
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-28 17:18:07 -0700

    Added more field validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
-  validates :title, presence: true, uniqueness:true
+  validates :title, presence: true
   validates :overview, presence: true
   validates :release_date, presence: true
   validates :total_inventory, presence: true
