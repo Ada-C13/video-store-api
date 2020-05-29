9b64c609aa497dd8e2af158cfd74bc59957bfae9
Yaz <yasmineoshaugh@gmail.com>
2020-05-29 13:11:04 -0700

    created testing fixtures for testing rental, updated rental tests. ALL PASSING

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,55 +1,40 @@
 require "test_helper"
 
 describe Rental do
-  before do 
-    @rental = Rental.new(customer_id: 1, video_id: 2)
-  end
+  let(:rental_fixture) {rental(:rental_1)}
+  let(:customer_fixture) {customer(:customer_1)}
+  let(:video_fixture) {video(:video_1)}
+  
   describe "valid creation" do
     it "must have assigned fields" do 
-      expect(@rental.customer_id).must_equal 1
-      expect(@rental.video_id).must_equal 2
+      expect(rental_fixture.customer_id).must_equal customer_fixture.id
+      expect(rental_fixture.video_id).must_equal video_fixture.id
     end
 
     it "must be connected to customer" do
-      customer = Customer.create(
-        name: "Shelley Rocha",
-        registered_at: "Wed, 29 Apr 2015 07:54:14 -0700",
-        address: "Ap #292-5216 Ipsum Rd.",
-        city: "Hillsboro",
-        state: "OR",
-        postal_code: "24309",
-        phone: "(322) 510-8695",
-        videos_checked_out_count: 1
-      )
-      @rental.customer = customer
-      expect(@rental.customer.id).must_equal customer.id
+      expect(rental_fixture.customer.id).must_equal customer_fixture.id
     end
 
     it "must be connected to video" do
-      video = Video.create(
-        title: "Rats and Birds And Strangers",
-        overview: "The childhood enemy claims the woman is heir to an incredible fortune, all paperwork has been taken care of already, all there's left to do is sign them. Distrustful of both this situation and of this childhood friend, the woman somewhat gladly agrees to the proposal, but there's no time to waste, a decision had to be made quickly.",
-        release_date: "2012-09-26",
-        total_inventory: 2,
-        available_inventory: 2
-      )
-      expect(@rental.video.id).must_equal video.id
+      expect(rental_fixture.video.id).must_equal video_fixture.id
     end
-    
-    it "must be valid" do 
-      result = @rental.valid?
+    # it "must be valid" do 
+    #   value(rental_fixture).must_be :valid?
+    # end
+    it "must be valid" do
+      result = rental_fixture.valid?
       expect(result).must_equal true
     end
   end
   describe "presence validation" do
     it "is invalid if customer_id is missing" do
-      @rental.customer_id = nil
-      result = @rental.valid?
+      rental_fixture.customer_id = nil
+      result = rental_fixture.valid?
       expect(result).must_equal false
     end
     it "is invalid if video_id is missing" do
-      @rental.video_id = nil
-      result = @rental.valid?
+      rental_fixture.video_id = nil
+      result = rental_fixture.valid?
       expect(result).must_equal false
     end
     
