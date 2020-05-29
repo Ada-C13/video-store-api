f1f04bf00e502650c78ee6c5d85aba6bbfca8292
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-29 00:34:11 -0700

    trying to get test to pass

--- app/controllers/rental_controller.rb
+++ app/controllers/rental_controller.rb
@@ -1,2 +1,30 @@
-class RentalController < ApplicationController
+class RentalsController < ApplicationController
+
+  def check_out
+    rental = Rental.check_out(@customer, @movie)
+    if rental 
+      render json: rental.as_json(only: [:id]), status: :ok
+      return
+    else
+      render json: { ok: false, errors: "Not Found"}, status: :not_found
+      return
+    end
+  end
+
+  def check_in
+    rental = Rental.check_in(@rental)
+    if rental
+      render json: rental.as_json(only: [:id]), status: :ok
+      return
+    else
+      render json: { ok: false, errors: "Not Found"}, status: :not_found
+      return
+    end
+  end
+
+  private
+  def rental_params
+    params.permit(:video_id, :customer_id)
+  end
+
 end
