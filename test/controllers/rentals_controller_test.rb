26f6a6cf90c0259dafd4c953017287ec9ab941b3
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-28 19:56:11 -0700

    added more tests for check_in action

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -8,7 +8,7 @@ describe RentalsController do
 
   describe 'check_out' do
     before do 
-      @customer = customers(:customer2)
+      @customer = customers(:customer1)
       @video = videos(:video1)
     end
     let(:rental_params) {
@@ -72,5 +72,63 @@ describe RentalsController do
       expect(rental.due_date).must_equal DateTime.current.to_date + 7
       must_respond_with :ok
     end
+  end
+
+  describe 'check_in' do
+    before do 
+      @customer = customers(:customer1)
+      @video = videos(:video1)
+    end
+    let(:rental_params) {
+      {
+        customer_id: @customer.id,
+        video_id: @video.id
+      }
+    }
+
+    it 'Can chech_in the rental' do
+      expect{ post check_in_path, params: rental_params}.wont_change "Rental.count"
+      must_respond_with :not_found
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash 
+    end
+
+    it "videos_checked_out_count should get decrease by one and available_inventory should get increase if we have a check_in " do
+      post check_in_path, params: rental_params
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+
+      customer = Customer.find_by(id: @customer.id)
+      expect(customer.videos_checked_out_count).must_equal 1 # 1 is in customer fixture
+
+      video = Video.find_by(id: @video.id)
+      expect(video.available_inventory).must_equal 9 #9 is in videos fixture
+    end
+
+    it "responde with 404 if video dosnt exist" do
+      rental_params[:video_id] = nil
+      expect{ post check_out_path, params: rental_params}.wont_change "Rental.count"
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body["errors"]).must_equal ["Not Found"]
+    end
+
+    it "responde with 404 if customer dosnt exist" do
+      rental_params[:customer_id] = nil
+      expect{ post check_in_path, params: rental_params}.wont_change "Rental.count"
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body["errors"]).must_equal ["Not Found"]
+    end
+
+
   end
 end
