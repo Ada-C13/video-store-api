3a834cde971106085bbee36f7cb7f1abb5c457a8
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-29 00:51:38 -0700

    verfied customers fields

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   def index 
-    customers = Customer.all.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone,  ])
+    customers = Customer.all.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, ])
     render json: customers, status: :ok
   end
 
