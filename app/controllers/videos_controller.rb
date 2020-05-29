073a124fac88d60409dde5386356634fd9938e74
Yaz <yasmineoshaugh@gmail.com>
2020-05-28 17:44:25 -0700

    videos controller with routes, tests, actions

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,35 @@
+class VideosController < ApplicationController
+  def index
+    videos = Video.all.as_json(only: [:id, :title, :release_date, :available_inventory])
+    render json: videos.as_json, status: :ok
+  end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+    else
+      render json: {
+        ok: false,
+        error: video.errors.messages
+      }, status: :bad_request
+    end
+  end
+
+  def show
+    video = Video.find_by(id: params[:id]).as_json
+    if video
+      render json: video.as_json([:id, :title, :release_date, :available_inventory, :total_inventory]), status: :ok
+    else
+      render json: {
+        ok: false,
+        error: video.errors.messages
+      }, status: :not_found
+    end
+  end
+
+  private
+  def video_params
+    return params.require(:video).permit(:id, :title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
+end
