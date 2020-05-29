47d7cf4b302158d93218094af4f9de1497acf6b5
Angela Nguyen <angethuy@gmail.com>
2020-05-28 21:49:29 -0700

    validations for video model all passing

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -8,8 +8,23 @@ describe Video do
       expect(video.valid?).must_equal true
     end
 
-    it "is invalid with a field is missing" do
+    it "is invalid when title is missing" do
       video.title = nil
+      expect(video.valid?).must_equal false
+    end
+
+    it "is invalid when overview is missing" do
+      video.overview = nil
+      expect(video.valid?).must_equal false
+    end
+
+    it "is invalid when release date is missing" do
+      video.release_date = nil
+      expect(video.valid?).must_equal false
+    end
+
+    it "is invalid when total inventory is missing" do
+      video.total_inventory = nil
       expect(video.valid?).must_equal false
     end
   end
