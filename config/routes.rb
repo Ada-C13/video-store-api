27aa674ba83977e5483c004320f13a961a63e5a1
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-28 17:18:22 -0700

    Added routes for check_out and check_in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :videos, only:[:index, :show, :create]
 
-  
+  post '/rentals/check-out', to: 'rentals#check_out', as: "checkout"
+  post '/rentals/check-in', to: 'rentals#check_in', as: "checkin"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
