d0032bf0449f845ae510f320892da65b3fdf671f
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-28 23:28:14 -0700

    added routes for cutomers and rentals

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- 
+  resources :customers, only: [:index]
   resources :videos, only: [:index, :show, :create]
+  resources :rentals, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
