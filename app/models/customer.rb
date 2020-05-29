10bea0b102e75bd3bfc64f54af8edcbec25eea96
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-29 00:37:16 -0700

    verifed/updated the relationships

---
+++ app/models/customer.rb
@@ -0,0 +1,15 @@
+class Customer < ApplicationRecord
+  has_many :rentals
+  has_many :videos, through: :rentals, dependent: :nullify
+
+  validates :name, presence: true
+  validates :registered_at, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :movies_checked_out_count, presence: true
+end
+
+
