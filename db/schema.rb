7c2216e5e4c35f44759b3d277ad945e56446cb31
Yaz <yasmineoshaugh@gmail.com>
2020-05-28 17:33:00 -0700

    set up video model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_28_052524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.string "release_date"
+    t.integer "total_inventory"
+    t.integer "available_inventory"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
