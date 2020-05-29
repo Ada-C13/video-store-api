92451cb2e222a74e33e04f7715fe546765c1a472
Haben <haben7@gmail.com>
2020-05-28 19:33:55 -0700

    added validation tests for rental

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
-describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe Rental do
+    describe "validation" do
+    let(:rental) { rentals(:rental_1) }
+
+    it "must be valid" do
+      expect(rental.valid?).must_equal true
+
+    end
+    it "wont be valid without a title" do
+      required_fields = [:customer_id, :video_id, :due_date, :checked_out_date]
+
+      rental = Rental.new
+      expect(rental.valid?).must_equal false
+      expect(rental.errors[:checked_in_date].count.zero?).must_equal true
+  
+      required_fields.each do |field|
+      expect(rental.errors[field].count.positive?).must_equal true
+      end
+
+    end
+  end 
 end
