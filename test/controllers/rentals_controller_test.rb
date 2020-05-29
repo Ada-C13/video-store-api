584405d1ffe6efde2599d09495b0b893b1dacaf2
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-28 19:20:52 -0700

    added more tests for checkout action

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -15,7 +15,7 @@ describe RentalsController do
       {
         customer_id: @customer.id,
         video_id: @video.id,
-        due_date: Date.today
+        due_date: DateTime.current.to_date
       }
     }
 
@@ -29,7 +29,7 @@ describe RentalsController do
       expect(body).must_be_instance_of Hash 
     end
 
-    it "Can not chech_out rental if video dosnt exist" do
+    it "responde with 404 if video dosnt exist" do
       rental_params[:video_id] = nil
       expect{ post check_out_path, params: rental_params}.wont_change "Rental.count"
       must_respond_with :not_found
@@ -39,11 +39,38 @@ describe RentalsController do
       expect(body["errors"]).must_equal ["Not Found"]
     end
 
+    it "responde with 404 if customer dosnt exist" do
+      rental_params[:customer_id] = nil
+      expect{ post check_out_path, params: rental_params}.wont_change "Rental.count"
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      expect(body["errors"]).must_equal ["Not Found"]
+    end
 
+    it "videos_checked_out_count should get increase by one and available_inventory should get decrease  if we have a check_out " do
+      post check_out_path, params: rental_params
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+
+      customer = Customer.find_by(id: @customer.id)
+      expect(customer.videos_checked_out_count).must_equal 2 # 1 is in customer fixture
+
+      video = Video.find_by(id: @video.id)
+      expect(video.available_inventory).must_equal 8 #9 is in videos fixture
+    end
+
+    it 'rental should will create du_date' do
+      post check_out_path, params: rental_params
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Hash
+      rental = Rental.first
+      expect(rental.due_date).must_equal DateTime.current.to_date + 7
+      must_respond_with :ok
+    end
   end
-
-
-
-
-
 end
