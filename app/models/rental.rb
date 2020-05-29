3bd8e79dd34122d23536b9237c17680f7e4e6e32
Haben <haben7@gmail.com>
2020-05-28 19:31:53 -0700

    added validations for rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,4 +2,9 @@ class Rental < ApplicationRecord
   belongs_to :video
   belongs_to :customer
 
+  validates :customer_id, presence: true
+  validates :video_id, presence: true
+  validates :due_date, presence: true
+  validates :checked_out_date, presence: true
+
 end
