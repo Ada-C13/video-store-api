b375d050aff091b761f4cfcdb4b18d41a3dfd0e2
Angela Nguyen <angethuy@gmail.com>
2020-05-28 21:51:29 -0700

    model validations for customer all passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -8,9 +8,36 @@ describe Customer do
       expect(customer.valid?).must_equal true
     end
 
-    it "is invalid with a field is missing" do
+    it "is invalid when name is missing" do
       customer.name = nil
       expect(customer.valid?).must_equal false
     end
+    
+    it "is invalid when address is missing" do
+      customer.address = nil
+      expect(customer.valid?).must_equal false
+    end
+
+    it "is invalid when name is missing" do
+      customer.city = nil
+      expect(customer.valid?).must_equal false
+    end
+
+    it "is invalid when name is missing" do
+      customer.state = nil
+      expect(customer.valid?).must_equal false
+    end
+
+    it "is invalid when name is missing" do
+      customer.postal_code = nil
+      expect(customer.valid?).must_equal false
+    end
+
+    it "is invalid when name is missing" do
+      customer.phone = nil
+      expect(customer.valid?).must_equal false
+    end
+
+
   end
 end
