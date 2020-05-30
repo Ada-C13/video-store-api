066b1f4e2a12f4d099a663e40274aefd75e0ea53
Yaz <yasmineoshaugh@gmail.com>
2020-05-29 18:50:26 -0700

    fixed customer controller action typos for rental post routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :videos, only: [:index, :show, :create]
   
-  post '/rentals/check-in', to: 'rentals#checkin', as: 'check_in'
-  post '/rentals/check-out', to: 'rentals#checkout', as: 'check_out'
+  post '/rentals/check-in', to: 'rentals#check_in', as: 'check_in'
+  post '/rentals/check-out', to: 'rentals#check_out', as: 'check_out'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
